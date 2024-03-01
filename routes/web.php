b2407a4995b63d24c0665d1437c6d4726d3c6a50
mdenitti <massimodn77@gmail.com>
2024-03-01 11:46:46 +0100

    basic data pass-on to the view from withing the web.php (routes) also basic array iteration with foreach

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $values= ["key1"=>"value1", "key2"=>"value2"];
+    $name = "massimo";
+    return view('welcome', compact('values', 'name'));
 });
