742a162eebffd8cd9d89b51caaab595db06f7808
mdenitti <massimodn77@gmail.com>
2024-03-04 13:39:09 +0100

    new db structure

---
+++ database/laravel11.sql
@@ -0,0 +1,78 @@
+-- -------------------------------------------------------------
+-- TablePlus 5.8.6(535)
+--
+-- https://tableplus.com/
+--
+-- Database: laravel11
+-- Generation Time: 2024-03-04 13:37:38.6020
+-- -------------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+CREATE TABLE `location_tday` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `location_id` int DEFAULT NULL,
+  `tday_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `locations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `tdays` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `location_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `location_tday` (`id`, `location_id`, `tday_id`) VALUES
+(1, 1, 1),
+(2, 2, 1);
+
+INSERT INTO `locations` (`id`, `name`) VALUES
+(1, 'Genk'),
+(2, 'Hasselt');
+
+INSERT INTO `tdays` (`id`, `name`, `date`, `created_at`, `updated_at`, `location_id`) VALUES
+(1, 'Zuid-Afrika', '2024-03-04 11:30:52', '2024-03-04 11:30:52', '2024-03-04 11:30:52', 1),
+(2, 'Spanje', '2024-03-04 11:30:52', '2024-03-04 11:30:52', '2024-03-04 11:30:52', 1),
+(3, 'Ivoorkust', '2024-03-04 11:30:52', '2024-03-04 11:30:52', '2024-03-04 11:30:52', 2),
+(4, 'Dubai', '2024-03-04 11:30:52', '2024-03-04 11:30:52', '2024-03-04 11:30:52', 2);
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(1, 'massimo', 'mas@mas.com'),
+(2, 'vladimir', 'Patatin@test.com'),
+(3, 'frank', 'frank@mail.com');
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
