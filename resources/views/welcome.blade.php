bf863ea030377a205363fbae0997a9f102b21c37
mdenitti <massimodn77@gmail.com>
2024-03-10 15:09:31 +0100

    css fix select, and added strtotime/date snippet for eu date display in select

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,13 +34,16 @@
 
     <h4>Selecteer een dag</h4>
 
-<select name="tday_location_id" id="tday_location_id"> 
+<select class="form-control" name="tday_location_id" id="tday_location_id"> 
     @foreach($tdays as $tday)
         @foreach($tday->locations as $location) 
-            <option value="{{ $location->id }}_{{ $tday->id }}">{{ $tday->name }} - {{ $location->name }}</option>
+            <option value="{{ $location->id }}_{{ $tday->id }}">
+                {{ date('d-m-Y', strtotime($tday->date)) }} - {{ $tday->name }} - {{ $location->name }}
+            </option>
         @endforeach
     @endforeach
 </select>
+
 
    
 
